34c9f89a8b8875fcc9a0d62b2159cac573780f41
Lenovo-05 <Lenovo-05@DESKTOP-E7JAN1Q>
2017-08-10 08:37:02 +0200

    Add project files.

---
+++ library/Låna.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace library
+{
+    /// <summary>
+    /// Interaction logic for Låna.xaml
+    /// </summary>
+    public partial class Låna : Window
+    {
+        public Låna win2;
+        public Låna(List<Book> basket)
+        {
+            InitializeComponent();
+
+            //MessageBox.Show(basket.ToString());
+            List<Book> localData = new List<Book>();
+            foreach (var item in basket)
+            { 
+
+            using (FSMbibliotekEntities data = new FSMbibliotekEntities())
+            {
+                    var res = from x in basket
+                              join booking in data.Bookings on x.Book_Id equals booking.Book_Id 
+                              join lib in data.Libraries on booking.Library_Id equals lib.Library_Id  
+                              //where x.Book_Id == booking.Book_Id 
+                              select new
+                              {
+                                booking.Book_Id, lib.Library_Name
+                              };
+
+
+                    foreach (var a in res)
+                    {
+                        //localData.Add(a);
+                    }
+                }
+
+                dataGrid.ItemsSource = localData;
+        }
+        }
+    }
+}
---
+++ library/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace library
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public List<Book> basket = new List<Book>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            List<Book> localData = new List<Book>();
+
+            using (FSMbibliotekEntities data = new FSMbibliotekEntities()) { 
+                var query = from m in data.Books
+                            where m.Book_Title.Contains(textBox.Text)
+                            select m;
+
+                foreach (var item in query)
+                {
+                    localData.Add(item);
+                }
+            }
+            dataGrid.ItemsSource = localData;
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            if (dataGrid.SelectedCells.Count > 0)
+            {
+                
+                basket.Add((Book)dataGrid.SelectedItem);
+
+                dataGrid1.ItemsSource = basket.ToList();
+            }
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+
+            Låna win2 = new Låna(basket);
+            win2.Show();
+            this.Close();
+
+        }
+    }
+    }
+
